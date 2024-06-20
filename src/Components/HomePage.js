3e44f0513d91304368fe69d16d60670b39f41de8
nayanatara07 <nayana2017tara@gmail.com>
2024-06-20 17:23:56 +0530

    login

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './HomePage.css'; 
+import './HomePage.css';
+import { FaUtensils } from 'react-icons/fa'; // Importing an icon from react-icons
 
 function HomePage() {
   return (
     <div className="home-page">
-      <h1 className="home-title">Welcome to Tara's Recipes Haven</h1>
-      <div className="login-button-container">
-        <Link to="/login">
-          <button className="login-button">Login</button>
-        </Link>
+      <div className="home-content">
+        <div className="home-icon">
+          <FaUtensils />
+        </div>
+        <h1 className="home-title">Welcome to Tara's Recipes Haven</h1>
+        <h2 className="home-subtitle">Discover delicious recipes and culinary tips</h2>
+        <div className="login-button-container">
+          <Link to="/login">
+            <button className="login-button">Login</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Login.css';
 
 const LoginPage = ({ onLogin }) => {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
   const navigate = useNavigate();
 
   const handleLogin = () => {
+    if (name === '' || password === '') {
+      setError('Name and password are required');
+      return;
+    }
+
+    // Add additional validation rules as needed
+    if (password.length < 6) {
+      setError('Password must be at least 6 characters long');
+      return;
+    }
+
+    setError('');
     onLogin();
     navigate('/app');
   };
@@ -12,6 +29,19 @@ const LoginPage = ({ onLogin }) => {
   return (
     <div className="form-container">
       <h2>Login Page</h2>
+      {error && <div className="error">{error}</div>}
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button onClick={handleLogin}>Login</button>
     </div>
   );
